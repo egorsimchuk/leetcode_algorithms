b62265330e0efe2f839f23233facad81d814f359
esimchuk <e.simchuk@conundrum.ai>
2022-12-07 01:08:11 +0300

    Fetch wonderful number
    Task: minimum-adjacent-swaps-to-reach-the-kth-smallest-number

--- src/medium/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.py
+++ src/medium/minimum-adjacent-swaps-to-reach-the-kth-smallest-number.py
@@ -2,31 +2,26 @@
 https://leetcode.com/problems/minimum-adjacent-swaps-to-reach-the-kth-smallest-number/description/
 """
 import bisect
+from itertools import combinations
+
 import numpy as np
 
 
 class Solution:
     def getMinSwaps(self, num: str, k: int) -> int:
-        wond_numbers_count = self.get_wonderful_counts(num, k)
-
-        wonderful_number = self.get_wonderful_number(wond_numbers_count, k)
-
-        res = 0
+        wond_numbers_count, sorted_arr = self.get_wonderful_counts(num, k)
+        wonderful_number = self.get_wonderful_number(sorted_arr, wond_numbers_count, k)
+        res = self.get_n_swaps(num, wonderful_number)
         return res
-
-    def get_wonderful_number(self, wond_numbers_count, k):
-        extra_count = wond_numbers_count - k
-        return
 
     def get_wonderful_counts(self, num, k):
         reveresed_num = num[::-1]
         wond_numbers = 0
         used_number_pos = 0
-        sorted_arr = [reveresed_num[used_number_pos]]
-        used_number_pos += 1
+        sorted_arr = []
         while wond_numbers < k:
             # update sorted arr
-            sorted_arr_pos = self.update_sorted_arr(sorted_arr, reveresed_num[used_number_pos])
+            sorted_arr_pos = self.update_sorted_arr(sorted_arr, int(reveresed_num[used_number_pos]))
             len_sorted_arr = len(sorted_arr)
             # skip if next number is equal to max (last) number in sorted_arr
             if sorted_arr_pos == len_sorted_arr:
@@ -42,7 +37,7 @@ class Solution:
                     sorted_arr_pos += 1
 
             used_number_pos += 1
-        return wond_numbers
+        return int(wond_numbers), sorted_arr
 
     def update_sorted_arr(self, sorted_arr, used_number):
         bisect.insort(sorted_arr, used_number)
@@ -50,8 +45,8 @@ class Solution:
         return sorted_arr_pos
 
     def count_permutations(self, sorted_arr, sorted_arr_pos):
-        sorted_arr_copy = sorted_arr.copy()
-        permutations_array = sorted_arr_copy.pop(sorted_arr_pos)
+        permutations_array = sorted_arr.copy()
+        permutations_array.pop(sorted_arr_pos)
 
         a = self.count_simple_permutations(len(permutations_array))
         uniq = np.unique(permutations_array, return_counts=True)[1]
@@ -63,7 +58,28 @@ class Solution:
     def count_simple_permutations(self, len):
         return np.prod(range(1, len + 1))
 
+    def get_wonderful_number(self, sorted_arr, wond_numbers_count, k):
+        extra_count = wond_numbers_count - k
+        sorted_arr_inv = sorted_arr[::-1]
+        sorted_arr_inv = [4, 3, 2, 2, 1]
+        pos = len(sorted_arr_inv) - 1
+        wonderful_numbers = []
+        while True:
+            sorted_arr_inv[pos]
+            right_part = sorted_arr_inv[pos + 1:]
+            if len(right_part) == 0:
+                pos -= 1
+                continue
+            argmax = pos + 1 + np.argmax(right_part)
+            sorted_arr_inv[pos], sorted_arr_inv[pos + 1] = sorted_arr_inv[argmax]
+        return
+
+    def get_n_swaps(self, from_num, to_num):
+        return
+
+
 if __name__ == '__main__':
     num = "5489355142"
     k = 3
-    Solution().getMinSwaps(num, k)
+    res = Solution().getMinSwaps(num, k)
+    assert res == 4
